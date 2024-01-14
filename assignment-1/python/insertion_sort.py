b4a57e6932074cedd85f776a218458b817de370d
Rasmus ØGLÆND <roglaend@gmail.com>
2024-01-14 17:52:26 +0100

    added steps to insertion sort

--- assignment-1/python/insertion_sort.py
+++ assignment-1/python/insertion_sort.py
@@ -3,25 +3,32 @@ import random as r
 
 def insertion_sort(list):
     n = len(list)
-    list = list[:]  # For not modifying the underlying original unsorted list
+    list = list[:]  # For not modifying the underlying original unsorted list'
+    steps = 0
 
     for i in range(1, n):
         key = list[i]
         j = i - 1
-
+        steps += 2
         while j >= 0 and key < list[j]:
             list[j + 1] = list[j]
             j -= 1
-
+            steps += 2
         list[j + 1] = key
-    return list
+        steps += 1
+    return list, steps
 
 
 if __name__ == "__main__":
-    num = 50
+    num = 10
 
     unsorted_list = [r.randint(1, num) for _ in range(num)]
     print("unsorted list: ", unsorted_list)
 
-    sorted_list = insertion_sort(unsorted_list)
+    sorted_list, steps = insertion_sort(unsorted_list)
     print("sorted list: ", sorted_list)
+
+
+    print("steps: ", steps)
+
+    print("expected steps: ", num * (num - 1) / 2 + 3 * (num - 1) + 1)
