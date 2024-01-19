94466175e852247ba346882c53969c3f5ca71660
Rasmus ØGLÆND <roglaend@gmail.com>
2024-01-19 15:34:09 +0100

    counting steps with plotting for quicksort

--- assignment-1/python/quicksort.py
+++ assignment-1/python/quicksort.py
@@ -1,14 +1,40 @@
 import random as r
+import math
+from matplotlib import pyplot as plt
+
+class QuickSort:
+    def __init__(self):
+        self.steps = 0
+    
+    def quicksort(self, list):
+        if len(list) <= 1:
+            return list
+
+        pivot = list[len(list) // 2]
+
+        left = []
+        middle = []
+        right = []
+
+        for num in list:
+            self.steps += 1
+            if num < pivot:
+                left.append(num)
+            elif num == pivot:
+                middle.append(num)
+            else:
+                right.append(num)
+
+        return self.quicksort(left) + middle + self.quicksort(right)
+
 
 
 def quicksort(list):
-    # base case
     if len(list) <= 1:
         return list
 
     pivot = list[len(list) // 2]
 
-    # Partition the list into three parts: less than, equal to, and greater than the pivot
     left = []
     middle = []
     right = []
@@ -25,19 +51,41 @@ def quicksort(list):
 
 
 if __name__ == "__main__":
-    num = 50
+    num = 6
 
     unsorted_list = [r.randint(1, num) for _ in range(num)]
     print("unsorted list: \n", unsorted_list)
 
-    sorted_list = quicksort(unsorted_list)
-    print("Sorted list: \n", sorted_list)
+    qs = QuickSort()
+    sorted_list = qs.quicksort(unsorted_list)
+    print("sorted list: \n", sorted_list)
+    print("steps: ", qs.steps)
+    print("expected steps: ", num * math.log(num, 2))
 
-    other_sorted_list = sorted(unsorted_list)
 
-    print("Other sorted list: \n", other_sorted_list)
+    # Plotting
+    points = 10
+    counted_steps = []
+    expected_steps = []
+    lengths = []
 
-    if other_sorted_list == sorted_list:
-        print("Equal")
-    else:
-        print("Not equal")
+    constant = 1.1
+
+    for i in range(points):
+        length = 2**i
+        unsorted_list = [r.randint(1, length) for _ in range(length)]
+        qs = QuickSort()
+        sorted = qs.quicksort(unsorted_list)
+        print("sorted list: \n", sorted)
+        counted_steps.append(qs.steps)
+        lengths.append(length)
+        expected = len(unsorted_list) * math.log(len(unsorted_list), 2) * constant
+        expected_steps.append(expected)
+
+
+    # Plotting
+    plt.figure(figsize=(10, 6))
+    plt.plot(lengths, counted_steps, label="counted steps")
+    plt.plot(lengths, expected_steps, label="expected steps")
+    plt.legend()
+    plt.show()
